c856cd2b7ea3d170f930fb0888b40e9f54bc8cc5
ABazing <jessbaze@gmail.com>
2025-02-24 18:10:49 +0100

    merge

---
+++ src/PokemonList.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+
+function PokemonList({ onSelect }) {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const pokeList = [];
+      for (let i = 1; i <= 151; i++) {
+        pokeList.push(fetch(`https://pokeapi.co/api/v2/pokemon/${i}`).then(res => res.json()));
+      }
+      const results = await Promise.all(pokeList);
+      setPokemons(results);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div className="pokemon-list">
+      {pokemons.map(pokelist => {
+        console.log(pokelist.name)
+        return(<h3>{pokelist.name}</h3>)
+      })}
+    </div>
+  );
+}
+
+export default PokemonList;
